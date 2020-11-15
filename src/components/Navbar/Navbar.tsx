955f6ed4ee2528dfb605f2141bb413809956fccb
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-11-15 10:55:20 -0300

    feature(Navbar): added features modal

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleMouseEnter = () => setIsModalVisible(true);
+
+  const handleMouseLeave = () => setIsModalVisible(false);
+
   return (
     <header>
-      <nav className='navbar__wrapper'>
-        <a href='?'>CompanyName</a>
-        <div>
-          <label htmlFor='search'>Search:</label>
-          <input placeholder='e.g. prices' />
+      <nav className='navbar__main-wrapper'>
+        <a href='/#'>CompanyName</a>
+        <input placeholder='Find features' />
+        <div className='navbar__modal-wrapper'>         
+          <button
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          > 
+            Feature #1  
+          </button>
+          {isModalVisible && (
+            <aside 
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}            
+              className='navbar__modal'
+            >
+              <a href='/#'>
+                Feature #1.1
+              </a>
+              <a href='/#'>
+                Feature #1.2
+              </a>
+              <a href='/#'>
+                Feature #1.3
+              </a>
+            </aside>
+          )}
         </div>
-        <span>Feature #1</span>
-        <span>Feature #2</span>
-        <button>Log In</button>
-        <button>Sign Up</button>
+        <a href='/#'>
+          Log In
+        </a>
+        <a href='/#'>
+          Sign Up
+        </a>
       </nav>
     </header>
   );
